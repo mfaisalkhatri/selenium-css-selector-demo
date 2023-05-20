9284fef3193c37ee0e2ef3a818a9b8f8dee62fab
mohammadfaisalkhatri@gmail.com <mohammadfaisalkhatri@gmail.com>
2023-05-20 14:28:41 +0530

    added syntax demo class

---
+++ src/test/java/io/github/mfaisalkhatri/pages/WebElement.java
@@ -0,0 +1,115 @@
+
+public class WebElements {
+
+private WebDriver driver;
+
+private WebElement selector () {
+    //  WebElement element = driver.findElement(By.cssSelector("<#><Value of ID attribute>"));
+
+    // WebElement   element = driver.findElement(By.cssSelector("#user-message"));
+
+    // WebElement element = driver.findElement(By.cssSelector("<HTML tag><#><Value of ID attribute>"));
+    
+    // WebElement element = driver.findElement(By.cssSelector("input#user-message"));
+
+    
+    // WebElement element = driver.findElement(By.cssSelector("<.><Class Name>"));
+    
+    // WebElement element = driver.findElement(By.cssSelector("“.text-header”"));
+
+    // WebElement element = driver.findElement(By.cssSelector("<.><Class Name>"));
+    
+    // WebElement element = driver.findElement(By.cssSelector(".text-header"));
+
+    // WebElement element = driver.findElement(By.cssSelector("<HTML tag><[name="value of attribute"]>"));
+    
+    // WebElement element = driver.findElement(By.cssSelector("input[name="userid"]"));
+
+    // WebElement element = driver.findElement(By.cssSelector("<HTML tag><[id=’value of attribute’]>[type=’value of attribute’]>"));
+    
+    //  WebElement element = driver.findElement(By.cssSelector("input[id=”submit”][type=”button”]"));
+
+
+    // WebElement element = driver.findElement(By.cssSelector("HTML tag><[value of tagName attribute]><.> <[class name]>"));
+    
+    // WebElement element = driver.findElement(By.cssSelector("button.submit-btn"));
+
+    // WebElement element = driver.findElement(By.cssSelector("<HTML tag><[attribute^='prefix of the string']>"));
+    
+    // WebElement element = driver.findElement(By.cssSelector("input[name^='user']"));
+
+    // WebElement element = driver.findElement(By.cssSelector("<HTML tag><[attribute$='suffix of the string']>"));
+    
+    // WebElement element = driver.findElement(By.cssSelector("input[id$='tle']"));
+
+
+    // WebElement element = driver.findElement(By.cssSelector("<HTML tag><[attribute*='Substring']>"));
+    
+    // WebElement element = driver.findElement(By.cssSelector("span[role*='box']"));
+
+
+    // WebElement element = driver.findElement(By.cssSelector("<HTML tag><[attribute~='word']>"));
+    
+    // WebElement element = driver.findElement(By.cssSelector("li[class~='select2-results__option']"));
+
+
+    // WebElement element = driver.findElement(By.cssSelector("<HTML Tag>:first-of-type"));
+    
+    // WebElement element = driver.findElement(By.cssSelector("#list>ul>li:first-of-type"));
+
+    // WebElement element = driver.findElement(By.cssSelector("<HTML Tag>:last-of-type"));
+    
+    // WebElement element = driver.findElement(By.cssSelector("#list>ul>li:last-of-type"));
+
+    // WebElement element = driver.findElement(By.cssSelector("<HTML Tag>:nth-of-type(n)"));
+    
+    // WebElement element = driver.findElement(By.cssSelector("#user-list>ul>li:nth-of-type(4)"));
+
+    // WebElement element = driver.findElement(By.cssSelector("<HTML Tag>:nth-child(n)"));
+    
+    // WebElement element = driver.findElement(By.cssSelector("#multi-select > option:nth-child(3)"));
+
+    // WebElement element = driver.findElement(By.cssSelector("<HTML Tag>:first-child"));
+    
+    // WebElement element = driver.findElement(By.cssSelector("#multi-select > option:first-child"));
+
+    // WebElement element = driver.findElement(By.cssSelector("<HTML Tag>:last-child"));
+    
+    // WebElement element = driver.findElement(By.cssSelector("#multi-select > option:last-child"));
+
+// <#><Value of ID attribute>
+
+// <HTML tag><#><Value of ID attribute>
+
+// <.><Value of Class attribute>
+
+// <HTML tag><.><Value of Class attribute>
+
+// <HTML tag><[attribute ='value of attribute']>
+
+// <HTML tag><[attribute^='prefix of the string']>
+
+// <HTML tag><[attribute$='suffix of the string']>
+
+// <HTML tag><[attribute*= 'Substring']>
+
+// <HTML tag><[attribute~='word']>
+
+// <HTML tag>:first-of-type
+
+// <HTML tag>:first-child
+
+// <HTML tag>:last-of-type
+
+// <HTML tag>:last-child
+
+// <HTML Tag>::nth-child(n)
+
+// <HTML Tag>:nth-of-type(n)
+
+
+}
+
+
+
+}
